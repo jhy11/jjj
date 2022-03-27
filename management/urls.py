460bf3e070174d9374866a63811928d3016dbb80
jin800812 <jin800812@gmail.com>
2022-03-27 23:12:41 +0900

    move qna page into management

--- management/urls.py
+++ management/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import info_shop_views, info_product_views, info_member_views, info_order_views
+from .views import info_shop_views, info_product_views, info_member_views, info_order_views, info_qna_views
 
 app_name = 'management'
 
@@ -12,5 +12,9 @@ urlpatterns=[
     path('order', info_order_views.OrderView.as_view(), name='order'),
     #path('order-data', info_order_views.PassData.as_view(), name='order-data')
     #path('order-detail', info_order_views.GetDetail.as_view(), name='order-detail'),
+
+    path('qna', info_qna_views.QnaView.as_view(), name='qna'),
+    path('qna-post/<str:id>', info_qna_views.QnaPostView.as_view(), name='qna-post'),
+    path('qna-edit/<str:id>', info_qna_views.QnaEditView.as_view(), name='qna-edit'),
 
 ]
