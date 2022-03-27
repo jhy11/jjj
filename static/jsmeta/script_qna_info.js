7bd3f05bc3d75420701c21c616627003bf5e24d1
jin800812 <jin800812@gmail.com>
2022-03-27 23:53:34 +0900

    Update management/qna

--- static/jsmeta/script_qna_info.js
+++ static/jsmeta/script_qna_info.js
@@ -1,8 +1,31 @@
-let table = $('#dataTableHover-qna').DataTable();
-table.columns(5).search( '미답변' ).draw();
+document.addEventListener("DOMContentLoaded", function(){
 
+  let status = document.getElementsByClassName('status');
+  [].forEach.call(status, function(status) {
+      status.innerHTML=showStatus(status.getAttribute('value'));
+  })
+
+  let table = $('#dataTableHover-qna').DataTable();
   
-$('#state').on('change', function () {
-  let state = $("input[name='state']:checked").val();
-  table.columns(5).search( state ).draw();
+  $('#Category').on('change', function () {
+    table.columns(0).search( this.value ).draw();
+  });
+  $('#Product').on('keyup', function () {
+    table.columns(1).search( this.value ).draw();
+  });
+  $('#Title').on('keyup', function () {
+    table.columns(3).search( this.value ).draw();
+  });
 });
+
+function showStatus(status){
+  switch(status){
+    case '1':
+      result = '<span class="badge badge-success align-middle">답변 완료</span>'
+      break;
+    case '0':
+      result = '<span class="badge badge-danger align-middle">미답변</span>'
+      break;
+  }
+  return result;
+}
